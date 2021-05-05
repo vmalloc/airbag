e09f2877f3c5f57877a32c0e4d37c940e62a7aac
Rotem Yaari <vmalloc@gmail.com>
2021-05-05 11:33:34 +0300

    Add airbag_if method

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,31 @@ mod utils;
 
 pub use dispatch::configure_pagerduty;
 
-pub trait AirbagResult: Sized {
+pub trait AirbagResult<E>: Sized {
     fn airbag_drop(self) {
         drop(self.airbag())
     }
 
+    fn airbag_if<F: Fn(&E) -> bool>(self, f: F) -> Self;
+
     fn airbag(self) -> Self;
 }
 
-impl<T, E: Debug + 'static> AirbagResult for Result<T, E> {
+impl<T, E: Debug + 'static> AirbagResult<E> for Result<T, E> {
     fn airbag(self) -> Self {
         if let Err(e) = &self {
             crate::dispatch::HUB
                 .read()
                 .dispatch(|| crate::alerts::generate_error_alert(e));
+        }
+        self
+    }
+
+    fn airbag_if<F: Fn(&E) -> bool>(self, f: F) -> Self {
+        if let Err(e) = &self {
+            if f(e) {
+                return self.airbag();
+            }
         }
         self
     }
